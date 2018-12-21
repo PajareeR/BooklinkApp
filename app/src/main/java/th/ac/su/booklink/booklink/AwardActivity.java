4b89d90c531c9c3e900fb5856dcc19dc1246abbd
Pajaree Ruangrit <pajaree2140@gmail.com>
2018-12-21 22:32:42 +0700

    text award list

--- app/src/main/java/th/ac/su/booklink/booklink/AwardActivity.java
+++ app/src/main/java/th/ac/su/booklink/booklink/AwardActivity.java
@@ -11,8 +11,8 @@ import java.util.ArrayList;
 public class AwardActivity extends AppCompatActivity {
 
     ListView simpleList;
-    String  Item[] = {"Apple", "Banana", "Lemon", "Cherry", "Strawberry", "Avocado"};
-    int flags[] = {R.drawable.sewrite, R.drawable.samakom, R.drawable.sevenbooks, R.drawable.nay, R.drawable.pan, R.drawable.vankeaw};
+    String  Item[] = {"รางวัลซีไรต์", "รางวัลหนังสือดีเด่น กระทรวงศึกษาธิการ", "รางวัลเซเว่นบุ๊คอวอร์ด", "รางวัลนายอินทร์อะวอร์ด", "รางวัลแว่นแก้ว", "รางวัลเรื่องสั้นมูลนิธิ สุภาว์ เทวกุล", "รางวัลพานแว่นฟ้า", "รางวัลสมาคมนักเขียนแห่งประเทศไทย"};
+    int flags[] = {R.drawable.sewrite, R.drawable.suksa, R.drawable.sevenbooks, R.drawable.nay, R.drawable.vankeaw, R.drawable.supa, R.drawable.pan, R.drawable.samakom};
 
 
     @Override
